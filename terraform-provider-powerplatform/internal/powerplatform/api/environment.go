859d88e39ca15acf6ac6a980f7b52e456fa1cc34
magodo <wztdyl@sina.com>
2023-05-11 10:46:07 +0800

    Review powerplatform_environment resource

--- terraform-provider-powerplatform/internal/powerplatform/api/environment.go
+++ terraform-provider-powerplatform/internal/powerplatform/api/environment.go
@@ -2,6 +2,7 @@ package powerplatform
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -26,11 +27,12 @@ func (client *Client) GetEnvironments() ([]Environment, error) {
 	return envs, nil
 }
 
-func (client *Client) GetEnvironment(id string) (*Environment, error) {
+func (client *Client) GetEnvironment(ctx context.Context, id string) (*Environment, error) {
 	request, error := http.NewRequest("GET", fmt.Sprintf("%s/api/environments/%s", client.HostURL, id), nil)
 	if error != nil {
 		return nil, error
 	}
+	request = request.WithContext(ctx)
 	body, error := client.doRequest(request)
 	if error != nil {
 		return nil, error
@@ -45,11 +47,12 @@ func (client *Client) GetEnvironment(id string) (*Environment, error) {
 	return &env, nil
 }
 
-func (client *Client) DeleteEnvironment(id string) error {
+func (client *Client) DeleteEnvironment(ctx context.Context, id string) error {
 	request, error := http.NewRequest("DELETE", fmt.Sprintf("%s/api/environments/%s", client.HostURL, id), nil)
 	if error != nil {
 		return error
 	}
+	request = request.WithContext(ctx)
 	_, error = client.doRequest(request)
 	if error != nil {
 		return error
@@ -58,7 +61,7 @@ func (client *Client) DeleteEnvironment(id string) error {
 	return nil
 }
 
-func (client *Client) CreateEnvironment(envToCreate EnvironmentCreate) (*Environment, error) {
+func (client *Client) CreateEnvironment(ctx context.Context, envToCreate EnvironmentCreate) (*Environment, error) {
 	body, error := json.Marshal(envToCreate)
 	if error != nil {
 		return nil, error
@@ -67,6 +70,7 @@ func (client *Client) CreateEnvironment(envToCreate EnvironmentCreate) (*Environ
 	if error != nil {
 		return nil, error
 	}
+	request = request.WithContext(ctx)
 	body, error = client.doRequest(request)
 	if error != nil {
 		return nil, error
@@ -81,7 +85,7 @@ func (client *Client) CreateEnvironment(envToCreate EnvironmentCreate) (*Environ
 	return &env, nil
 }
 
-func (client *Client) UpdateEnvironment(id string, envToUpdate Environment) error {
+func (client *Client) UpdateEnvironment(ctx context.Context, id string, envToUpdate Environment) error {
 	body, error := json.Marshal(envToUpdate)
 	if error != nil {
 		return error
@@ -90,6 +94,7 @@ func (client *Client) UpdateEnvironment(id string, envToUpdate Environment) erro
 	if error != nil {
 		return error
 	}
+	request = request.WithContext(ctx)
 	_, error = client.doRequest(request)
 	if error != nil {
 		return error
