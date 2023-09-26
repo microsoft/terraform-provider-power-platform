210bf98ebd70032d5c19e20e15b8b71987f8793e
Eduardo Sanchez <eduardodfmex@hotmail.com>
2023-09-26 12:31:09 -0600

    Adding TF files for test.

---
+++ examples/data-sources/powerplatform_applicationusers/data-source.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    powerplatform = {
+      version = "0.2"
+      source  = "microsoft/power-platform"
+    }
+  }
+}
+
+provider "powerplatform" {
+  client_id = var.client_id
+  secret    = var.secret
+  tenant_id = var.tenant_id
+}
+
+data "powerplatform_applicationusers" "all_applicationusers" {
+  environment_id = var.environment_id
+}
---
+++ examples/data-sources/powerplatform_applicationusers/outputs.tf
@@ -0,0 +1,4 @@
+output "all_applicationusers" {
+  description = "Returns all Solutions in an environment"
+  value       = data.powerplatform_applicationusers.all_applicationusers
+}
