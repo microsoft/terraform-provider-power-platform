8a4bd44e430cae58951124ab249ff0a54c93bc0d
Eduardo Sanchez <eduardodfmex@hotmail.com>
2023-09-27 14:41:43 -0600

    Updating TF files removing User and PWD on the samples that dont use it.

--- examples/data-sources/powerplatform_connectors/data-source.tf
+++ examples/data-sources/powerplatform_connectors/data-source.tf
@@ -8,9 +8,11 @@ terraform {
 }
 
 provider "powerplatform" {
+  client_id = var.client_id
+  secret    = var.secret
+  tenant_id = var.tenant_id
   username  = var.username
   password  = var.password
-  tenant_id = var.tenant_id
 }
 
 data "powerplatform_connectors" "all_connectors" {}
