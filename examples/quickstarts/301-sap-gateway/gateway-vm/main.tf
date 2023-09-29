8882de6fb43c3678b0af4c507cdf48f6162527f2
Flavio Furlan <flaviofurlan@microsoft.com>
2023-09-29 17:09:20 +0000

    Organization

--- examples/quickstarts/301-sap-gateway/gateway-vm/main.tf
+++ examples/quickstarts/301-sap-gateway/gateway-vm/main.tf
@@ -59,7 +59,7 @@ resource "azurerm_virtual_machine_extension" "install_ps7" {
 
   settings = <<SETTINGS
 {
-   "commandToExecute": "powershell -command \"[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('${base64encode(data.template_file.ps7.rendered)}')) | Out-File -filepath ps7.ps1\" | powershell -ExecutionPolicy RemoteSigned -File ps7.ps1 -AdmincredsUserName ${data.template_file.ps7.vars.AdmincredsUserName} -AdmincredsPassword ${data.template_file.ps7.vars.AdmincredsPassword}"
+   "commandToExecute": "powershell -command \"[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('${base64encode(data.template_file.ps7.rendered)}')) | Out-File -filepath ps7.ps1\" | powershell -ExecutionPolicy Unrestricted -File ps7.ps1 -AdmincredsUserName ${data.template_file.ps7.vars.AdmincredsUserName} -AdmincredsPassword ${data.template_file.ps7.vars.AdmincredsPassword}"
 }
 SETTINGS
   #"commandToExecute": "powershell -command \"[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('${base64encode(data.template_file.ps7.rendered)}')) | Out-File -filepath ps7.ps1\" && powershell -ExecutionPolicy Unrestricted -File ps7.ps1 -AdmincredsUserName ${data.template_file.ps7.vars.AdmincredsUserName} -AdmincredsPassword ${data.template_file.ps7.vars.AdmincredsPassword}"
@@ -67,7 +67,7 @@ SETTINGS
 
 #Variable input for the powershell7-setup.ps1 script
 data "template_file" "ps7" {
-  template = file("./gateway-vm/powershell7-setup.ps1")
+  template = file("./gateway-vm/scripts/installps7.ps1")
 
   vars = {
     AdmincredsUserName = "sapadmin"
