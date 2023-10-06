a87230c5b2e06f011968437754e314b551a143b7
Eduardo Sanchez <eduardodfmex@hotmail.com>
2023-10-05 18:36:02 -0600

    Update internal/powerplatform/resource_environment_test.go
    Co-authored-by: mawasile <50197777+mawasile@users.noreply.github.com>

--- internal/powerplatform/resource_environment_test.go
+++ internal/powerplatform/resource_environment_test.go
@@ -155,7 +155,7 @@ func TestUnitEnvironmentsResource_Validate_Create_And_Force_Recreate(t *testing.
 
 			}`,
 			Check: resource.ComposeTestCheckFunc(
-				resource.TestCheckResourceAttr("powerplatform_environment.development", "environment_id", envIdBeforeChanges),
+				resource.TestCheckResourceAttr("powerplatform_environment.development", "id", envIdBeforeChanges),
 				resource.TestCheckResourceAttr("powerplatform_environment.development", "location", "europe"),
 				resource.TestCheckResourceAttr("powerplatform_environment.development", "currency_code", "USD"),
 			),
