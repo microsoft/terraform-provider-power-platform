bf5ffd91fff06a8863d4b9f758ded921ca5d5ce8
Eduardo Sanchez <eduardodfmex@hotmail.com>
2023-10-11 11:59:06 -0600

    Updating powerapp dto.

--- internal/powerplatform/datasource_powerapps.go
+++ internal/powerplatform/datasource_powerapps.go
@@ -38,18 +38,18 @@ type PowerAppsListDataSourceModel struct {
 }
 
 type PowerAppsDataSourceModel struct {
-	EnvironmentName types.String `tfsdk:"id"`
-	DisplayName     types.String `tfsdk:"display_name"`
-	Name            types.String `tfsdk:"name"`
-	CreatedTime     types.String `tfsdk:"created_time"`
+	EnvironmentId types.String `tfsdk:"id"`
+	DisplayName   types.String `tfsdk:"display_name"`
+	Name          types.String `tfsdk:"name"`
+	CreatedTime   types.String `tfsdk:"created_time"`
 }
 
 func ConvertFromPowerAppDto(powerAppDto models.PowerAppBapi) PowerAppsDataSourceModel {
 	return PowerAppsDataSourceModel{
-		EnvironmentName: types.StringValue(powerAppDto.Properties.Environment.Name),
-		DisplayName:     types.StringValue(powerAppDto.Properties.DisplayName),
-		Name:            types.StringValue(powerAppDto.Name),
-		CreatedTime:     types.StringValue(powerAppDto.Properties.CreatedTime),
+		EnvironmentId: types.StringValue(powerAppDto.Properties.Environment.Id),
+		DisplayName:   types.StringValue(powerAppDto.Properties.DisplayName),
+		Name:          types.StringValue(powerAppDto.Name),
+		CreatedTime:   types.StringValue(powerAppDto.Properties.CreatedTime),
 	}
 }
 
