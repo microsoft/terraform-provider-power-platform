8b9bcc69c0c56983c88a41407eaae48968007474
Eduardo Sanchez <eduardodfmex@hotmail.com>
2023-11-02 14:55:51 -0600

    creating TestUnitManagedEnvironmentsResource_Validate_Create(

--- internal/powerplatform/resource_managed_environment_test.go
+++ internal/powerplatform/resource_managed_environment_test.go
@@ -328,15 +328,14 @@ func TestAccManagedEnvironmentsResource_Validate_Update(t *testing.T) {
 	})
 }
 
-// Implementing Unit test for managed environment Validate, Create and Update to be managed
+// Implementing Unit test for managed environment Validate, Create and Update to be managed ManagedEnvironmentsResource_Validate_Create
 
-func TestUnitEnvironmentsResource_Validate_Create_And_UpdatetoManaged(t *testing.T) {
+func TestUnitManagedEnvironmentsResource_Validate_Create(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
 	mock_helpers.ActivateOAuthHttpMocks()
 	mock_helpers.ActivateEnvironmentHttpMocks()
 
-	getLifecycleResponseInx := 0
 	patchResponseInx := 0
 
 	// Http Mock for managed environment
@@ -344,36 +343,21 @@ func TestUnitEnvironmentsResource_Validate_Create_And_UpdatetoManaged(t *testing
 		func(req *http.Request) (*http.Response, error) {
 			return httpmock.NewStringResponse(http.StatusOK, httpmock.File(fmt.Sprintf("services/environment/tests/resource/Validate_Create_And_Update/get_environments_%d.json", patchResponseInx)).String()), nil
 		})
+	httpmock.RegisterResponder("POST", "https://api.bap.microsoft.com/providers/Microsoft.BusinessAppPlatform/environments/00000000-0000-0000-0000-000000000001/governanceConfiguration?api-version=2021-04-01",
+		func(req *http.Request) (*http.Response, error) {
+			resp := httpmock.NewStringResponse(http.StatusAccepted, "")
+			resp.Header.Add("Location", "https://europe.api.bap.microsoft.com/providers/Microsoft.BusinessAppPlatform/lifecycleOperations/b03e1e6d-73db-4367-90e1-2e378bf7e2fc?api-version=2023-06-01")
+			return resp, nil
+		})
 
 	httpmock.RegisterResponder("GET", "https://europe.api.bap.microsoft.com/providers/Microsoft.BusinessAppPlatform/lifecycleOperations/b03e1e6d-73db-4367-90e1-2e378bf7e2fc?api-version=2023-06-01",
 		func(req *http.Request) (*http.Response, error) {
-			return httpmock.NewStringResponse(http.StatusOK, httpmock.File(fmt.Sprintf("services/managed_environment/tests/resource/Validate_Create_And_Update/get_lifecycle_%d.json", getLifecycleResponseInx)).String()), nil
+			return httpmock.NewStringResponse(http.StatusOK, httpmock.File("services/managed_environment/tests/resource/Validate_Create_And_Update/get_lifecycle.json").String()), nil
 		})
-	/*
-		httpmock.RegisterResponder("POST", "https://api.bap.microsoft.com/providers/Microsoft.BusinessAppPlatform/environments?api-version=2023-06-01",
-			func(req *http.Request) (*http.Response, error) {
-				getLifecycleResponseInx++
-				resp := httpmock.NewStringResponse(http.StatusAccepted, "")
-				resp.Header.Add("Location", "https://europe.api.bap.microsoft.com/providers/Microsoft.BusinessAppPlatform/lifecycleOperations/b03e1e6d-73db-4367-90e1-2e378bf7e2fc?api-version=2023-06-01")
-				return resp, nil
-			})
-
-		httpmock.RegisterResponder("GET", `=~^https://api\.bap\.microsoft\.com/providers/Microsoft\.BusinessAppPlatform/scopes/admin/environments/([\d-]+)\z`,
-			func(req *http.Request) (*http.Response, error) {
-				return httpmock.NewStringResponse(http.StatusOK, httpmock.File(fmt.Sprintf("services/environment/tests/resource/Validate_Create_And_Update/get_environment_%d.json", patchResponseInx)).String()), nil
-			})
-
-		httpmock.RegisterResponder("PATCH", `=~^https://api\.bap\.microsoft\.com/providers/Microsoft\.BusinessAppPlatform/scopes/admin/environments/([\d-]+)\z`,
-			func(req *http.Request) (*http.Response, error) {
-				patchResponseInx++
-				return httpmock.NewStringResponse(http.StatusAccepted, ""), nil
-			})
-
-		httpmock.RegisterResponder("GET", "https://api.bap.microsoft.com/providers/Microsoft.BusinessAppPlatform/scopes/admin/environments?api-version=2023-06-01",
-			func(req *http.Request) (*http.Response, error) {
-				return httpmock.NewStringResponse(http.StatusOK, httpmock.File(fmt.Sprintf("services/environment/tests/resource/Validate_Create_And_Update/get_environments_%d.json", patchResponseInx)).String()), nil
-			})
-	*/
+	httpmock.RegisterResponder("GET", "https://api.bap.microsoft.com/providers/Microsoft.BusinessAppPlatform/scopes/admin/environments/00000000-0000-0000-0000-000000000001?%24expand=permissions%2Cproperties.capacity&api-version=2023-06-01",
+		func(req *http.Request) (*http.Response, error) {
+			return httpmock.NewStringResponse(http.StatusOK, httpmock.File("services/managed_environment/tests/resource/Validate_Create_And_Update/get_environment_create_response.json").String()), nil
+		})
 
 	resource.Test(t, resource.TestCase{
 		IsUnitTest:               true,
@@ -394,159 +378,18 @@ func TestUnitEnvironmentsResource_Validate_Create_And_UpdatetoManaged(t *testing
 				}`,
 
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestMatchResourceAttr("powerplatform_environment.development", "id", regexp.MustCompile(powerplatform_helpers.GuidRegex)),
-				),
-			},
-			{
-				Config: UnitTestsProviderConfig + `
-				resource "powerplatform_managed_environment" "managed_development" {
-					environment_id             = "00000000-0000-0000-0000-000000000001"
-					is_usage_insights_disabled = false
-					is_group_sharing_disabled  = true
-					limit_sharing_mode         = "ExcludeSharingToSecurityGroups"
-					max_limit_user_sharing     = 10
-					solution_checker_mode      = "None"
-					suppress_validation_emails = true
-					maker_onboarding_markdown  = "this is test markdown"
-					maker_onboarding_url       = "http://www.example.com"
-				}`,
+					// Run test for specific environment one time
+					resource.TestCheckResourceAttr("powerplatform_managed_environment.managed_development", "id", "00000000-0000-0000-0000-000000000001"),
 
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestMatchResourceAttr("powerplatform_environment.development", "id", regexp.MustCompile(powerplatform_helpers.GuidRegex)),
-					resource.TestCheckResourceAttr("powerplatform_managed_environment.managed_development", "is_usage_insights_disabled", "false"),
-				),
-			},
-			{
-				Config: UnitTestsProviderConfig + `
-				resource "powerplatform_managed_environment" "managed_development" {
-					environment_id             = "00000000-0000-0000-0000-000000000001"
-					is_usage_insights_disabled = false
-					is_group_sharing_disabled  = false
-					limit_sharing_mode         = "ExcludeSharingToSecurityGroups"
-					max_limit_user_sharing     = 10
-					solution_checker_mode      = "None"
-					suppress_validation_emails = true
-					maker_onboarding_markdown  = "this is test markdown"
-					maker_onboarding_url       = "http://www.example.com"
-				}`,
-
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestMatchResourceAttr("powerplatform_environment.development", "id", regexp.MustCompile(powerplatform_helpers.GuidRegex)),
-					resource.TestCheckResourceAttr("powerplatform_managed_environment.managed_development", "is_group_sharing_disabled", "false"),
-				),
-			},
-			{
-				Config: UnitTestsProviderConfig + `
-				resource "powerplatform_managed_environment" "managed_development" {
-					environment_id             = "00000000-0000-0000-0000-000000000001"
-					is_usage_insights_disabled = false
-					is_group_sharing_disabled  = false
-					limit_sharing_mode         = "NoLimit"
-					max_limit_user_sharing     = 10
-					solution_checker_mode      = "None"
-					suppress_validation_emails = true
-					maker_onboarding_markdown  = "this is test markdown"
-					maker_onboarding_url       = "http://www.example.com"
-				}`,
-
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestMatchResourceAttr("powerplatform_environment.development", "id", regexp.MustCompile(powerplatform_helpers.GuidRegex)),
-					resource.TestCheckResourceAttr("powerplatform_managed_environment.managed_development", "limit_sharing_mode", "NoLimit"),
-				),
-			},
-			{
-				Config: AcceptanceTestsProviderConfig + `
-				resource "powerplatform_managed_environment" "managed_development" {
-					environment_id             = "00000000-0000-0000-0000-000000000001"
-					is_usage_insights_disabled = false
-					is_group_sharing_disabled  = false
-					limit_sharing_mode         = "NoLimit"
-					max_limit_user_sharing     = -1
-					solution_checker_mode      = "None"
-					suppress_validation_emails = true
-					maker_onboarding_markdown  = "this is test markdown"
-					maker_onboarding_url       = "http://www.example.com"
-				}`,
-
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestMatchResourceAttr("powerplatform_environment.development", "id", regexp.MustCompile(powerplatform_helpers.GuidRegex)),
-					resource.TestCheckResourceAttr("powerplatform_managed_environment.managed_development", "max_limit_user_sharing", "-1"),
-				),
-			},
-			{
-				Config: UnitTestsProviderConfig + `
-				resource "powerplatform_managed_environment" "managed_development" {
-					environment_id             = "00000000-0000-0000-0000-000000000001"
-					is_usage_insights_disabled = false
-					is_group_sharing_disabled  = false
-					limit_sharing_mode         = "NoLimit"
-					max_limit_user_sharing     = -1
-					solution_checker_mode      = "None"
-					suppress_validation_emails = false
-					maker_onboarding_markdown  = "this is test markdown"
-					maker_onboarding_url       = "http://www.example.com"
-				}`,
-
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestMatchResourceAttr("powerplatform_environment.development", "id", regexp.MustCompile(powerplatform_helpers.GuidRegex)),
-					resource.TestCheckResourceAttr("powerplatform_managed_environment.managed_development", "suppress_validation_emails", "false"),
-				),
-			},
-			{
-				Config: UnitTestsProviderConfig + `
-				resource "powerplatform_managed_environment" "managed_development" {
-					environment_id             = "00000000-0000-0000-0000-000000000001"
-					is_usage_insights_disabled = false
-					is_group_sharing_disabled  = false
-					limit_sharing_mode         = "NoLimit"
-					max_limit_user_sharing     = -1
-					solution_checker_mode      = "None"
-					suppress_validation_emails = false
-					maker_onboarding_markdown  = "this is test markdown changed"
-					maker_onboarding_url       = "http://www.example.com"
-				}`,
-
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestMatchResourceAttr("powerplatform_environment.development", "id", regexp.MustCompile(powerplatform_helpers.GuidRegex)),
-					resource.TestCheckResourceAttr("powerplatform_managed_environment.managed_development", "maker_onboarding_markdown", "this is test markdown changed"),
-				),
-			},
-			{
-				Config: UnitTestsProviderConfig + `
-				resource "powerplatform_managed_environment" "managed_development" {
-					environment_id             = "00000000-0000-0000-0000-000000000001"
-					is_usage_insights_disabled = false
-					is_group_sharing_disabled  = false
-					limit_sharing_mode         = "NoLimit"
-					max_limit_user_sharing     = -1
-					solution_checker_mode      = "None"
-					suppress_validation_emails = false
-					maker_onboarding_markdown  = "this is test markdown changed"
-					maker_onboarding_url       = "http://www.example-changed.com"
-				}`,
-
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestMatchResourceAttr("powerplatform_environment.development", "id", regexp.MustCompile(powerplatform_helpers.GuidRegex)),
-					resource.TestCheckResourceAttr("powerplatform_managed_environment.managed_development", "maker_onboarding_url", "http://www.example-changed.com"),
-				),
-			},
-			{
-				Config: UnitTestsProviderConfig + `
-				resource "powerplatform_managed_environment" "managed_development" {
-					environment_id             = "00000000-0000-0000-0000-000000000001"
-					is_usage_insights_disabled = false
-					is_group_sharing_disabled  = false
-					limit_sharing_mode         = "NoLimit"
-					max_limit_user_sharing     = -1
-					solution_checker_mode      = "Warn"
-					suppress_validation_emails = false
-					maker_onboarding_markdown  = "this is test markdown changed"
-					maker_onboarding_url       = "http://www.example-changed.com"
-				}`,
-
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestMatchResourceAttr("powerplatform_environment.development", "id", regexp.MustCompile(powerplatform_helpers.GuidRegex)),
-					resource.TestCheckResourceAttr("powerplatform_managed_environment.managed_development", "solution_checker_mode", "Warn"),
+					resource.TestCheckResourceAttr("powerplatform_managed_environment.managed_development", "is_usage_insights_disabled", "true"),
+					resource.TestCheckResourceAttr("powerplatform_managed_environment.managed_development", "protection_level", "Standard"),
+					resource.TestCheckResourceAttr("powerplatform_managed_environment.managed_development", "is_group_sharing_disabled", "true"),
+					resource.TestCheckResourceAttr("powerplatform_managed_environment.managed_development", "limit_sharing_mode", "ExcludeSharingToSecurityGroups"),
+					resource.TestCheckResourceAttr("powerplatform_managed_environment.managed_development", "max_limit_user_sharing", "10"),
+					resource.TestCheckResourceAttr("powerplatform_managed_environment.managed_development", "solution_checker_mode", "None"),
+					resource.TestCheckResourceAttr("powerplatform_managed_environment.managed_development", "suppress_validation_emails", "true"),
+					resource.TestCheckResourceAttr("powerplatform_managed_environment.managed_development", "maker_onboarding_markdown", "this is test markdown"),
+					resource.TestCheckResourceAttr("powerplatform_managed_environment.managed_development", "maker_onboarding_url", "http://www.example.com"),
 				),
 			},
 		},
