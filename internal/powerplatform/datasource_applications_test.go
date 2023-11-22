681b061e3a121a42656f1f735f91e4914c8cc836
Mateusz Wasilewski <50197777+mawasile@users.noreply.github.com>
2023-11-22 17:09:21 +0000

    Add unit test for ApplicationsDataSource_Validate_Read

--- internal/powerplatform/datasource_applications_test.go
+++ internal/powerplatform/datasource_applications_test.go
@@ -1,11 +1,14 @@
 package powerplatform
 
 import (
+	"net/http"
 	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/jarcoal/httpmock"
 	powerplatform_helpers "github.com/microsoft/terraform-provider-power-platform/internal/powerplatform/helpers"
+	mock_helpers "github.com/microsoft/terraform-provider-power-platform/internal/powerplatform/mocks"
 )
 
 func TestAccApplicationsDataSource_Validate_Read(t *testing.T) {
@@ -36,6 +39,42 @@ func TestAccApplicationsDataSource_Validate_Read(t *testing.T) {
 					resource.TestMatchResourceAttr("data.powerplatform_applications.all_applications", "applications.0.application_id", regexp.MustCompile(powerplatform_helpers.GuidRegex)),
 					resource.TestMatchResourceAttr("data.powerplatform_applications.all_applications", "applications.0.application_name", regexp.MustCompile(powerplatform_helpers.StringRegex)),
 					resource.TestMatchResourceAttr("data.powerplatform_applications.all_applications", "applications.0.unique_name", regexp.MustCompile(powerplatform_helpers.StringRegex)),
+				),
+			},
+		},
+	})
+}
+
+func TestUnitApplicationsDataSource_Validate_Read(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+	mock_helpers.ActivateOAuthHttpMocks()
+
+	httpmock.RegisterResponder("GET", `https://api.powerplatform.com/appmanagement/environments/00000000-0000-0000-0000-000000000001/applicationPackages?api-version=2022-03-01-preview`,
+		func(req *http.Request) (*http.Response, error) {
+			return httpmock.NewStringResponse(http.StatusOK, httpmock.File("services/application/tests/datasource/Validate_Read/get_applications.json").String()), nil
+		})
+
+	resource.Test(t, resource.TestCase{
+		IsUnitTest:               true,
+		ProtoV6ProviderFactories: TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: UnitTestsProviderConfig + `
+				data "powerplatform_applications" "all_applications" {
+					environment_id = "00000000-0000-0000-0000-000000000001"
+				}`,
+
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestMatchResourceAttr("data.powerplatform_applications.all_applications", "id", regexp.MustCompile(`^[1-9]\d*$`)),
+					resource.TestCheckResourceAttr("data.powerplatform_applications.all_applications", "environment_id", "00000000-0000-0000-0000-000000000001"),
+					resource.TestCheckResourceAttr("data.powerplatform_applications.all_applications", "applications.#", "2"),
+					resource.TestCheckResourceAttr("data.powerplatform_applications.all_applications", "applications.0.application_id", "4bbd5362-21f6-47a8-bcd9-e2a75e8242ef"),
+					resource.TestCheckResourceAttr("data.powerplatform_applications.all_applications", "applications.0.application_name", "Dynamics 365 Customer Voice"),
+					resource.TestCheckResourceAttr("data.powerplatform_applications.all_applications", "applications.0.unique_name", "MicrosoftFormsPro"),
+					resource.TestCheckResourceAttr("data.powerplatform_applications.all_applications", "applications.1.application_id", "f50a3059-435a-401b-a7ee-1bca67da5657"),
+					resource.TestCheckResourceAttr("data.powerplatform_applications.all_applications", "applications.1.application_name", "Intelligent Order Management Portal"),
+					resource.TestCheckResourceAttr("data.powerplatform_applications.all_applications", "applications.1.unique_name", "msdyn_IOMOrderReturnsPortalAnchor"),
 				),
 			},
 		},
