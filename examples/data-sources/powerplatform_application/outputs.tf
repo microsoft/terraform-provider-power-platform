03f639856c591132dcac39c976cd2ae0cbba72fa
polatengin <enpolat@microsoft.com>
2023-11-23 01:50:59 +0000

    removing unneeded comment lines

--- examples/data-sources/powerplatform_application/outputs.tf
+++ examples/data-sources/powerplatform_application/outputs.tf
@@ -7,20 +7,3 @@ output "all_applications" {
   description = "All applications"
   value       = data.powerplatform_applications.all_applications
 }
-
-/*
-output "name" {
-  description = "Display name of the environment"
-  value       = powerplatform_application.development.display_name
-}
-
-output "id" {
-  description = "Unique identifier of the environment"
-  value       = powerplatform_application.development.environment_name
-}
-
-output "url" {
-  description = "URL of the environment"
-  value       = powerplatform_application.development.url
-}
-*/
