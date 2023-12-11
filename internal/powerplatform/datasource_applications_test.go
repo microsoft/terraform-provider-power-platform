95f5eb3c4a79e855e6b3f8f1ff44d5f268942361
polatengin <enpolat@microsoft.com>
2023-12-11 22:26:37 +0000

    updating domain name

--- internal/powerplatform/datasource_applications_test.go
+++ internal/powerplatform/datasource_applications_test.go
@@ -24,7 +24,7 @@ func TestAccApplicationsDataSource_Validate_Read(t *testing.T) {
 					language_code     = "1033"
 					currency_code     = "USD"
 					environment_type  = "Sandbox"
-					domain            = "applicationacceptancetest"
+					domain            = "testaccapplicationsdatasourcevalidateread"
 					security_group_id = "00000000-0000-0000-0000-000000000000"
 				}
 
