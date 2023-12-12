579e6ce2da522ce6a322f4ed635c6de87648bb2b
polatengin <enpolat@microsoft.com>
2023-12-12 23:18:02 +0000

    adding more tests

--- internal/powerplatform/resource_application_test.go
+++ internal/powerplatform/resource_application_test.go
@@ -47,6 +47,33 @@ func TestAccApplicationResource_Validate_Install(t *testing.T) {
 					resource.TestCheckResourceAttr("powerplatform_application.development", "unique_name", "ProcessMiningAnchor"),
 				),
 			},
+			{
+				Config: AcceptanceTestsProviderConfig + `
+				resource "powerplatform_environment" "environment" {
+					display_name                              = "` + envDomain + `"
+					location                                  = "europe"
+					language_code                             = "1033"
+					currency_code                           = "USD"
+					environment_type                          = "Sandbox"
+					domain = "` + envDomain + `"
+					security_group_id = "00000000-0000-0000-0000-000000000000"
+				}
+				data "powerplatform_applications" "application_to_install" {
+					environment_id = data.powerplatform_environments.all_environments.environments[0].id
+					name           = "Dynamics 365 Customer Voice"
+					publisher_name = "Microsoft Dynamics 365"
+				}
+				resource "powerplatform_application" "development" {
+					environment_id = powerplatform_environment.environment.id
+  				unique_name = data.powerplatform_applications.application_to_install.applications[0].unique_name
+				}`,
+
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestMatchResourceAttr("powerplatform_application.development", "id", regexp.MustCompile(powerplatform_helpers.GuidRegex)),
+					resource.TestMatchResourceAttr("powerplatform_application.development", "environment_id", regexp.MustCompile(powerplatform_helpers.GuidRegex)),
+					resource.TestCheckResourceAttr("powerplatform_application.development", "unique_name", "MicrosoftFormsPro"),
+				),
+			},
 		},
 	})
 }
@@ -77,6 +104,14 @@ func TestUnitApplicationResource_Validate_Install(t *testing.T) {
 		},
 	)
 
+	httpmock.RegisterResponder("POST", "https://api.powerplatform.com/appmanagement/environments/00000000-0000-0000-0000-000000000000/applicationPackages/MicrosoftFormsPro/install?api-version=2022-03-01-preview",
+		func(req *http.Request) (*http.Response, error) {
+			resp := httpmock.NewStringResponse(http.StatusAccepted, "")
+			resp.Header.Add("Operation-Location", "https://api.powerplatform.com/appmanagement/environments/402c2b45-f5dc-e561-869f-368544f94a13/operations/475af49d-9bca-437f-8be1-9e467f44be8a?api-version=1")
+			return resp, nil
+		},
+	)
+
 	resource.Test(t, resource.TestCase{
 		IsUnitTest:               true,
 		ProtoV6ProviderFactories: TestAccProtoV6ProviderFactories,
@@ -92,6 +127,18 @@ func TestUnitApplicationResource_Validate_Install(t *testing.T) {
 					resource.TestMatchResourceAttr("powerplatform_application.development", "id", regexp.MustCompile(powerplatform_helpers.StringRegex)),
 					resource.TestMatchResourceAttr("powerplatform_application.development", "environment_id", regexp.MustCompile(powerplatform_helpers.GuidRegex)),
 					resource.TestCheckResourceAttr("powerplatform_application.development", "unique_name", "ProcessMiningAnchor"),
+				),
+			},
+			{
+				Config: UnitTestsProviderConfig + `
+				resource "powerplatform_application" "development" {
+					environment_id   = "00000000-0000-0000-0000-000000000000"
+					unique_name      = "MicrosoftFormsPro"
+				}`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestMatchResourceAttr("powerplatform_application.development", "id", regexp.MustCompile(powerplatform_helpers.StringRegex)),
+					resource.TestMatchResourceAttr("powerplatform_application.development", "environment_id", regexp.MustCompile(powerplatform_helpers.GuidRegex)),
+					resource.TestCheckResourceAttr("powerplatform_application.development", "unique_name", "MicrosoftFormsPro"),
 				),
 			},
 		},
