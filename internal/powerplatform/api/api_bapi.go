e5dc4fcfa635c5c6255b4fceca43a3fdb5d0829e
Mateusz Wasilewski <50197777+mawasile@users.noreply.github.com>
2024-01-29 10:30:39 +0100

    merge changes

--- internal/powerplatform/api/api_bapi.go
+++ internal/powerplatform/api/api_bapi.go
@@ -56,7 +56,6 @@ type LifecycleRequestedByDto struct {
 	Type        string `json:"type"`
 }
 
-
 func NewBapiClientApi(baseApi *ApiClientBase, auth *BapiAuth, dataverseClient *DataverseClientApi) *BapiClientApi {
 	return &BapiClientApi{
 		baseApi:         baseApi,
@@ -79,45 +78,6 @@ func (client *BapiClientApi) Execute(ctx context.Context, method string, url str
 		return nil, err
 	}
 	return client.baseApi.ExecuteBase(ctx, token, method, url, headers, body, acceptableStatusCodes, responseObj)
-}
-
-func (client *BapiClientApi) DoWaitForLifecycleOperationStatus(ctx context.Context, response *ApiHttpResponse) (*LifecycleDto, error) {
-
-	locationHeader := response.GetHeader("Location")
-	tflog.Debug(ctx, "Location Header: "+locationHeader)
-
-	_, err := url.Parse(locationHeader)
-	if err != nil {
-		tflog.Error(ctx, "Error parsing location header: "+err.Error())
-	}
-
-	retryHeader := response.GetHeader("Retry-After")
-	tflog.Debug(ctx, "Retry Header: "+retryHeader)
-	retryAfter, err := time.ParseDuration(retryHeader)
-	if err != nil {
-		retryAfter = time.Duration(5) * time.Second
-	} else {
-		retryAfter = retryAfter * time.Second
-	}
-
-	for {
-		lifecycleResponse := LifecycleDto{}
-		response, err = client.Execute(ctx, "GET", locationHeader, nil, nil, []int{http.StatusOK}, &lifecycleResponse)
-		if err != nil {
-			return nil, err
-		}
-
-		time.Sleep(retryAfter)
-
-		tflog.Debug(ctx, "Environment Creation Operation State: '"+lifecycleResponse.State.Id+"'")
-		tflog.Debug(ctx, "Environment Creation Operation HTTP Status: '"+response.Response.Status+"'")
-
-		if lifecycleResponse.State.Id == "Succeeded" {
-			return &lifecycleResponse, nil
-		} else if lifecycleResponse.State.Id == "Failed" {
-			return &lifecycleResponse, errors.New("environment creation failed. provisioning state: " + lifecycleResponse.State.Id)
-		}
-	}
 }
 
 func (client *BapiClientApi) DoWaitForLifecycleOperationStatus(ctx context.Context, response *ApiHttpResponse) (*LifecycleDto, error) {
