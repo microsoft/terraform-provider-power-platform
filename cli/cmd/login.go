63bd8a7fbb78a587deef1c22d06a5e26ebbbfb2c
mawasile <50197777+mawasile@users.noreply.github.com>
2024-02-06 14:52:48 +0100

    Update cli/cmd/login.go
    Co-authored-by: Matt Dotson <mattdot@users.noreply.github.com>

--- cli/cmd/login.go
+++ cli/cmd/login.go
@@ -50,7 +50,7 @@ Command: terraform-provider-power-platform login
 			authResults, err = cli.DeviceCodeLogin(ctx, &tenantId, constants.REQUIRED_SCOPES, fileCache)
 
 		} else if cmd.Flag("user").Changed && cmd.Flag("password").Changed {
-			username := cmd.Flag("username").Value.String()
+			username := cmd.Flag("user").Value.String()
 			pass := cmd.Flag("password").Value.String()
 			authResults, err = cli.UserPassLogin(ctx, &tenantId, &username, &pass, constants.REQUIRED_SCOPES, fileCache)
 
