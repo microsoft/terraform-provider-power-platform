91dbba60395a60ae0436add5031a218cfb0bdf3e
Mateusz Wasilewski <50197777+mawasile@users.noreply.github.com>
2024-03-21 08:40:00 +0000

    Update ProtoV6ProviderFactories in TestUnitLocationsDataSource_Validate_Read

--- internal/powerplatform/datasource_locations_test.go
+++ internal/powerplatform/datasource_locations_test.go
@@ -44,7 +44,7 @@ func TestUnitLocationsDataSource_Validate_Read(t *testing.T) {
 
 	resource.Test(t, resource.TestCase{
 		IsUnitTest:               true,
-		ProtoV6ProviderFactories: TestAccProtoV6ProviderFactories,
+		ProtoV6ProviderFactories: TestUnitTestProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
 				Config: TestsProviderConfig + `
