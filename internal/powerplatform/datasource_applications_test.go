04bbe658ccf634f95e44aaf98818988fd54ad120
Mateusz Wasilewski <50197777+mawasile@users.noreply.github.com>
2024-03-26 10:11:13 +0000

    Fix file paths in test cases

--- internal/powerplatform/datasource_applications_test.go
+++ internal/powerplatform/datasource_applications_test.go
@@ -59,7 +59,7 @@ func TestUnitApplicationsDataSource_Validate_Read(t *testing.T) {
 
 	httpmock.RegisterResponder("GET", `https://api.powerplatform.com/appmanagement/environments/00000000-0000-0000-0000-000000000001/applicationPackages?api-version=2022-03-01-preview`,
 		func(req *http.Request) (*http.Response, error) {
-			return httpmock.NewStringResponse(http.StatusOK, httpmock.File("services/application/tests/datasource/Validate_Read/get_applications.json").String()), nil
+			return httpmock.NewStringResponse(http.StatusOK, httpmock.File("services/application/tests/datasource/environment_application_packages/Validate_Read/get_applications.json").String()), nil
 		})
 
 	resource.Test(t, resource.TestCase{
