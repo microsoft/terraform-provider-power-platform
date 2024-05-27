5f2cd0fa0a7015fb24f1e69610e203e6c603053d
Mateusz Wasilewski <50197777+mawasile@users.noreply.github.com>
2024-05-27 13:29:15 +0000

    feat: Add error handling for object not found in GetEnvironment
    The code changes in `api_environment.go` and `resource_environment.go` add error handling for the case when an environment with a specific ID is not found. If the error message contains "404", it wraps the error into a provider error and returns it. This ensures that the appropriate error message is displayed when an environment is not found.

--- internal/powerplatform/services/environment/api_environment.go
+++ internal/powerplatform/services/environment/api_environment.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	api "github.com/microsoft/terraform-provider-power-platform/internal/powerplatform/api"
+	powerplatform_helpers "github.com/microsoft/terraform-provider-power-platform/internal/powerplatform/helpers"
 	solution "github.com/microsoft/terraform-provider-power-platform/internal/powerplatform/services/solution"
 )
 
@@ -235,6 +236,9 @@ func (client *EnvironmentClient) GetEnvironment(ctx context.Context, environment
 	env := EnvironmentDto{}
 	_, err := client.Api.Execute(ctx, "GET", apiUrl.String(), nil, nil, []int{http.StatusOK}, &env)
 	if err != nil {
+		if strings.ContainsAny(err.Error(), "404") {
+			return nil, powerplatform_helpers.WrapIntoProviderError(err, powerplatform_helpers.ERROR_OBJECT_NOT_FOUND, fmt.Sprintf("environment '%s' not found", environmentId))
+		}
 		return nil, err
 	}
 
