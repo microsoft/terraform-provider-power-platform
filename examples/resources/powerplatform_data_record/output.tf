e759cd9cb3f62ed3d55a26305b99cd931d86f86e
Engin Polat <polatengin@hotmail.com>
2024-06-05 20:47:01 +0000

    updating output terraform file

--- examples/resources/powerplatform_data_record/output.tf
+++ examples/resources/powerplatform_data_record/output.tf
@@ -1,14 +1,9 @@
-output "data_record_sample_contact1" {
-  description = "Sample Contact 1 Data Record"
-  value       = resource.powerplatform_data_record.data_record_sample_contact1
+output "data_record_role" {
+  description = "Role Data Record"
+  value       = resource.powerplatform_data_record.role
 }
 
-output "data_record_sample_contact2" {
-  description = "Sample Contact 2 Data Record"
-  value       = resource.powerplatform_data_record.data_record_sample_contact2
-}
-
-output "data_record_accounts" {
-  description = "Accounts Data Record"
-  value       = resource.powerplatform_data_record.data_record_accounts
+output "data_record_team" {
+  description = "Team Data Record"
+  value       = resource.powerplatform_data_record.team
 }
