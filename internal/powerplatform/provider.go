3a8996a28a5ff1642e9fa1a257b273078b69ce55
Ian Jensen <94021216+ianjensenisme@users.noreply.github.com>
2024-06-14 00:12:45 -0600

    Ianjensenisme/339 bad log output (#340)
    * Removing log output
    
    * Removing inaccurate log output
    
    ---------
    
    Co-authored-by: mawasile <50197777+mawasile@users.noreply.github.com>

--- internal/powerplatform/provider.go
+++ internal/powerplatform/provider.go
@@ -193,7 +193,6 @@ func (p *PowerPlatformProvider) Configure(ctx context.Context, req provider.Conf
 	oidcRequestUrl := ""
 	envOidcRequestUrl := MultiEnvDefaultFunc([]string{"ARM_OIDC_REQUEST_URL", "ACTIONS_ID_TOKEN_REQUEST_URL"})
 	if config.OidcRequestUrl.IsNull() {
-		tflog.Debug(ctx, "OIDC request URL environment variable is null")
 		oidcRequestUrl = envOidcRequestUrl
 	} else {
 		oidcRequestUrl = config.OidcRequestUrl.ValueString()
@@ -202,7 +201,6 @@ func (p *PowerPlatformProvider) Configure(ctx context.Context, req provider.Conf
 	oidcRequestToken := ""
 	envOidcRequestToken := MultiEnvDefaultFunc([]string{"ARM_OIDC_REQUEST_TOKEN", "ACTIONS_ID_TOKEN_REQUEST_TOKEN"})
 	if config.OidcRequestToken.IsNull() {
-		tflog.Debug(ctx, "OIDC request token environment variable is null")
 		oidcRequestToken = envOidcRequestToken
 	} else {
 		oidcRequestToken = config.OidcRequestToken.ValueString()
@@ -211,7 +209,6 @@ func (p *PowerPlatformProvider) Configure(ctx context.Context, req provider.Conf
 	oidcToken := ""
 	envOidcToken := EnvDefaultFunc("ARM_OIDC_TOKEN", "")
 	if config.OidcToken.IsNull() {
-		tflog.Debug(ctx, "OIDC token environment variable is null")
 		oidcToken = envOidcToken
 	} else {
 		oidcToken = config.OidcToken.ValueString()
