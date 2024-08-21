6f3a05f0ae53cae36a4b107f5093f1d119a5c444
mawasile <50197777+mawasile@users.noreply.github.com>
2024-08-21 08:53:07 +0000

    fixing policy name test

--- internal/powerplatform/services/licensing/resource_billing_policy_test.go
+++ internal/powerplatform/services/licensing/resource_billing_policy_test.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"net/http"
 	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -36,7 +37,7 @@ func TestAccBillingPolicyResource_Validate_Create(t *testing.T) {
 				}
 
 				resource "powerplatform_billing_policy" "pay_as_you_go" {
-					name     = "` + mocks.TestName() + `"
+					name     = "` + strings.ReplaceAll(mocks.TestName(), "_", "") + `"
 					location = "europe"
 					status   = "Enabled"
 					billing_instrument = {
@@ -47,7 +48,7 @@ func TestAccBillingPolicyResource_Validate_Create(t *testing.T) {
 				`,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestMatchResourceAttr("powerplatform_billing_policy.pay_as_you_go", "id", regexp.MustCompile(helpers.GuidRegex)),
-					resource.TestCheckResourceAttr("powerplatform_billing_policy.pay_as_you_go", "name", mocks.TestName()),
+					resource.TestCheckResourceAttr("powerplatform_billing_policy.pay_as_you_go", "name", strings.ReplaceAll(mocks.TestName(), "_", "")),
 					resource.TestCheckResourceAttr("powerplatform_billing_policy.pay_as_you_go", "location", "europe"),
 					resource.TestCheckResourceAttr("powerplatform_billing_policy.pay_as_you_go", "status", "Enabled"),
 					resource.TestCheckResourceAttr("powerplatform_billing_policy.pay_as_you_go", "billing_instrument.resource_group", "power-platform-billing-"+mocks.TestName()),
