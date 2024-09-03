837adadea99a6c2f61c7089efcb3ee98055f5174
mawasile <50197777+mawasile@users.noreply.github.com>
2024-09-03 07:39:27 +0000

    fixing dev container

--- .devcontainer/features/pac/install.sh
+++ .devcontainer/features/pac/install.sh
@@ -5,7 +5,7 @@ echo "Activating feature 'Power Platform CLI'"
 
 # Install .NET Core SDK
 sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-7.0
+  sudo apt-get install -y dotnet-sdk-8.0
 
 # Install Power Platform CLI to vscode user's path
 dotnet tool install Microsoft.PowerApps.CLI.Tool --interactive false --tool-path /home/vscode/.dotnet/tools
