80fea97567725df0abffaa284617671781c3ce85
Matt Dotson <mattdot@users.noreply.github.com>
2024-09-04 09:02:12 -0700

    Apply suggestions from code review

--- examples/resources/powerplatform_data_loss_prevention_policy/import.sh
+++ examples/resources/powerplatform_data_loss_prevention_policy/import.sh
@@ -1,2 +1,2 @@
-# DLP Policies can be imported using the DLP policy id
+# DLP Policies can be imported using the DLP policy id (replace with a real DLP Policy guid)
 terraform import powerplatform_data_loss_prevention_policy.example_dlp 00000000-0000-0000-0000-000000000000
--- examples/resources/powerplatform_environment/import.sh
+++ examples/resources/powerplatform_environment/import.sh
@@ -1,2 +1,2 @@
-# Environment resource can be imported using the environment id id, e.g.
+# Environment resource can be imported using the environment id (replace with a real environment guid)
 terraform import powerplatform_environment.example 00000000-0000-0000-0000-000000000000
