050a43cab1e712ac22237ee9f998ae05077133b6
mawasile <50197777+mawasile@users.noreply.github.com>
2024-10-02 21:04:39 +0200

    Added disable_on_destroy on powerplatform_data_record (#480)
    * adding disable_on_destroy logic
    
    * Add `disable_on_destroy` attribute to powerplatform_record
    
    * Refactor user resource test to use element and split functions for aad_id
    
    * Update provider version constraints
    
    * Refactor user resource test to use element and split functions for aad_id
    
    * PR fixes
    
    ---------
    
    Co-authored-by: github-actions[bot] <tfmod442916@users.noreply.github.com>

--- examples/resources/powerplatform_data_record/res_business_unit/main.tf
+++ examples/resources/powerplatform_data_record/res_business_unit/main.tf
@@ -25,6 +25,7 @@ variable "parent_business_unit_id" {
 resource "powerplatform_data_record" "business_unit" {
   environment_id     = var.environment_id
   table_logical_name = "businessunit"
+  disable_on_destroy = true
   columns = {
     name       = var.name
     costcenter = var.costcenter
@@ -33,26 +34,6 @@ resource "powerplatform_data_record" "business_unit" {
       data_record_id     = var.parent_business_unit_id
     }
   }
-}
-
-data "powerplatform_environments" "envs" {
-}
-
-locals {
-  dataverse_url = tostring(one([for e in data.powerplatform_environments.envs.environments : e.dataverse.url if e.id == var.environment_id]))
-}
-
-resource "powerplatform_rest" "business_unit_disable_on_destroy" {
-  destroy = {
-    scope                = "${local.dataverse_url}.default"
-    method               = "PATCH"
-    url                  = "${local.dataverse_url}api/data/v9.2/businessunits(${powerplatform_data_record.business_unit.id})"
-    expected_http_status = [200, 204]
-    body = jsonencode({
-      isdisabled    = true
-    })
-  }
-  depends_on = [powerplatform_data_record.business_unit]
 }
 
 output "resource_id" {
