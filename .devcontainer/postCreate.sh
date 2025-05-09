106a7b16279a9e98a4d9eaa499a2502e42faeb07
Mateusz Wasilewski <mawasile@microsoft.com>
2025-05-09 16:03:55 +0000

    feat: add postCreate script for GitHub CLI extension installation

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -eux
+
+# Turn commits auto sign to auto
+# https://docs.github.com/en/authentication/managing-commit-signature-verification/signing-commits
+#git config commit.gpgsign true
+
+#Install codeql extension
+echo "Installing GitHub CodeQL CLI extension..."
+gh extensions install github/gh-codeql
