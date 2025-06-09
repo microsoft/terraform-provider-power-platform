c2bbec3017d6fe938ec00ec224279e44c2af3250
Matt Dotson <mattdot@users.noreply.github.com>
2025-06-09 01:58:02 -0700

    Add partner_id setting to provider (#781)
    * feat: add partner_id configuration
    
    * Update .changes/unreleased/added-20250517-062944.yaml
    
    * Update internal/provider/provider.go
    
    * Update internal/provider/provider.go
    
    ---------
    
    Co-authored-by: mawasile <50197777+mawasile@users.noreply.github.com>

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,6 +6,7 @@ package config
 import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/cloud"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/microsoft/terraform-provider-power-platform/internal/customtypes"
 	"github.com/microsoft/terraform-provider-power-platform/internal/helpers"
 )
 
@@ -52,11 +53,13 @@ type ProviderConfig struct {
 	EnableContinuousAccessEvaluation bool
 
 	// internal runtime configuration values
-	TestMode         bool
-	Urls             ProviderConfigUrls
-	TelemetryOptout  bool
-	Cloud            cloud.Configuration
-	TerraformVersion string
+	TestMode                  bool
+	Urls                      ProviderConfigUrls
+	TelemetryOptout           bool
+	PartnerId                 string
+	DisableTerraformPartnerId bool
+	Cloud                     cloud.Configuration
+	TerraformVersion          string
 }
 
 type ProviderConfigUrls struct {
@@ -135,8 +138,10 @@ type ProviderConfigModel struct {
 	UseOidc types.Bool `tfsdk:"use_oidc"`
 	UseMsi  types.Bool `tfsdk:"use_msi"`
 
-	Cloud           types.String `tfsdk:"cloud"`
-	TelemetryOptout types.Bool   `tfsdk:"telemetry_optout"`
+	Cloud                     types.String     `tfsdk:"cloud"`
+	TelemetryOptout           types.Bool       `tfsdk:"telemetry_optout"`
+	PartnerId                 customtypes.UUID `tfsdk:"partner_id"`
+	DisableTerraformPartnerId types.Bool       `tfsdk:"disable_terraform_partner_id"`
 
 	TenantId           types.String `tfsdk:"tenant_id"`
 	AuxiliaryTenantIDs types.List   `tfsdk:"auxiliary_tenant_ids"`
