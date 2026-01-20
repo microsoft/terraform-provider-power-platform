a419521949ac8683b84798c34d60e84c111f8387
Mateusz Wasilewski <50197777+mawasile@users.noreply.github.com>
2026-01-20 23:07:19 +0100

    Update comment for tenant settings import script

--- examples/resources/powerplatform_tenant_settings/import.sh
+++ examples/resources/powerplatform_tenant_settings/import.sh
@@ -1,3 +1,3 @@
-# Environment resource can be imported using the tenant id (replace with a real tenant id)
+# Tenant settings resource can be imported using the tenant id (replace with a real tenant id)
 terraform import powerplatform_tenant_settings.example 00000000-0000-0000-0000-000000000000
 
