b3b2ae3aebe558cc76ebdade4302a8d02b48c2f1
Mateusz Wasilewski <50197777+mawasile@users.noreply.github.com>
2026-01-20 23:05:38 +0100

    Update comment for application package import instruction

--- examples/resources/powerplatform_environment_application_package_install/import.sh
+++ examples/resources/powerplatform_environment_application_package_install/import.sh
@@ -1,3 +1,3 @@
-# Environment application package install resource can be imported using the environment application package install id (replace with a real environment application package install in format {environment_id}_{unique_name})
+# Application package install resource can be imported using the environment application package install id (replace with a real environment application package install in format {environment_id}_{unique_name})
 terraform import powerplatform_environment_application_package_install.example 00000000-0000-0000-0000-000000000000_example-package-unique-name
 
